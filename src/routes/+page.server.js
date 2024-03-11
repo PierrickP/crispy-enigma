128acc8122cf46679fb7f7fac7204acbd48142b3
Pierrick <mulotmail@gmail.com>
2024-03-11 16:21:57 +0100

    knex to postgres
    Signed-off-by: Pierrick <mulotmail@gmail.com>

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,15 +1,13 @@
-import knex from "knex";
+import postgres from "postgres";
 
-const db = knex({
-  client: "pg",
-  connection:
-    "postgres://reader:NWDMCE5xdipIjRrp@hh-pgsql-public.ebi.ac.uk:5432/pfmegrnargs",
-});
+const db = postgres(
+  "postgres://reader:NWDMCE5xdipIjRrp@hh-pgsql-public.ebi.ac.uk:5432/pfmegrnargs"
+);
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
   return {
     now: new Date(),
-    data: db("rnc_database").select(db.raw("NOW(), count(*)")).first(),
+    data: db`SELECT NOW(), count(*) FROM rnc_database LIMIT 1`,
   };
 }
