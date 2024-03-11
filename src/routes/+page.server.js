85b208fd9e0252a978c5f804378f96a685cd4b80
Pierrick <mulotmail@gmail.com>
2024-03-11 16:35:38 +0100

    Init db on load()
    Signed-off-by: Pierrick <mulotmail@gmail.com>

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,11 +1,11 @@
 import postgres from "postgres";
 
-const db = postgres(
-  "postgres://reader:NWDMCE5xdipIjRrp@hh-pgsql-public.ebi.ac.uk:5432/pfmegrnargs"
-);
-
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
+  const db = postgres(
+    "postgres://reader:NWDMCE5xdipIjRrp@hh-pgsql-public.ebi.ac.uk:5432/pfmegrnargs"
+  );
+
   return {
     now: new Date(),
     data: db`SELECT NOW(), count(*) FROM rnc_database LIMIT 1`,
